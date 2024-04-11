abb58c3db350030c96af8c670cb87d07f7ae67a7
GabrielJ <gaboluca.jimenez@gmail.com>
2024-04-10 23:01:42 -0600

    cambios en login

--- src/main/java/org/example/facturacion/presentation/Controllers/MainController.java
+++ src/main/java/org/example/facturacion/presentation/Controllers/MainController.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
     @GetMapping("/")
+   // public String getMainPage() {
+       // return "redirect:/presentation/login";
+    //}
     public String getMainPage() {
-        return "redirect:/presentation/login";
+        return "presentation/perfil";
     }
 }
